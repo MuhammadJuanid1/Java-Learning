da63534832b7f8a42a2e7d1e54f872d0d65c69cd
Muhammad Junaid <114782540+MuhammadJuanid1@users.noreply.github.com>
2022-11-09 08:38:13 +0000

    Add files via upload

---
+++ class FirstClass {.java
@@ -0,0 +1,17 @@
+import javax.print.event.PrintEvent;
+
+class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello World With Java\nFrom Apna College\n");
+        System.out.print("Junaid Kashmiri\n");
+        // Qs.Print the pattern
+        // Answer
+        System.out.println("*\n**\n***\n****\n");
+        
+        
+        
+        
+    }
+    
+}
---
+++ main.java
@@ -0,0 +1,39 @@
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+import java.util.*;
+
+public class main{
+    
+    public static void main(String[] args){
+        // variables
+        // int a = 35;
+        // int b = 25;
+        // int sum = a + b;
+        // System.out.println(sum);
+    //    int mul = a * b;
+    //    System.out.println(mul);
+    // Quiz Karke Dikhao
+    // To calculate a * b / a - b
+    // int a = 10;
+    // int b = 5;
+    // int mul = a * b;
+    // int sub = a - b;
+    // int divide = mul / sub;
+    // System.out.println(mul);
+    // System.out.println(sub);
+    // System.out.println(divide);
+    // Input /
+    // Scanner sc = new Scanner(System.in);
+    // String name = sc.nextLine();
+    // System.out.println(name);
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println(sum);
+
+
+
+    
+    }
+}
